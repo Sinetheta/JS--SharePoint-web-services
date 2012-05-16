89365e504cf10de96c15a28ba6b01f7dbe1e40e9
Kevin Attfield <k.attfield@gmail.com>
2012-05-16 12:48:03 -0700

    added scopes

---
+++ search/GetPortalSearchInfo.js
@@ -0,0 +1,31 @@
+$.SP = {};
+$.SP.server = 'https://yourserver';    
+$.SP.Search = {};
+
+$.SP.Search.GetPortalSearchInfo = function () {
+    var soap = '';
+            
+    soap += '<soap12:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap12="http://www.w3.org/2003/05/soap-envelope">';
+    soap += '   <soap12:Body>';
+    soap += '       <GetPortalSearchInfo xmlns="http://microsoft.com/webservices/OfficeServer/QueryService" />';
+    soap += '   </soap12:Body>';
+    soap += '</soap12:Envelope>';
+    
+    return $.ajax({
+        type: "POST",
+        contentType: "text/xml;charset='utf-8'",
+        url: $.SP.server + '/_vti_bin/Search.asmx',
+        data: soap,
+        dataType: "xml"
+    });
+}
+
+// Example use
+$.when( $.SP.Search.GetPortalSearchInfo() ).done(function(data, textStatus, jqXHR){
+    var escapedScopes = $(data).find('GetPortalSearchInfoResult').text();
+    var scopes = $(escapedScopes).find('Name');
+    scopes = $.map(scopes, function(scope){
+        return $(scope).text();
+    });
+    console.log(scopes);
+});
